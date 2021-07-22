98ee2f3f5029ebfae608d858d02aef5cf9a953fa
sabdesamie <abdesamieselama@gmail.com>
2021-07-22 22:02:49 +0100

    front and backend basics

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const getIndex = (req,res)=>{
+	res.render('index');
+}
+
+const postIndex = (req,res)=> {
+	console.log(req.body.title);
+	res.redirect('/');
+}
+
+module.exports = {
+	getIndex:getIndex,
+	postIndex:postIndex
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const indexController = require('../controllers/index')
 
-router.get("/",(req,res)=>{
-	res.render('index');
-});
+router.get("/",indexController.getIndex);
+
+router.post("/ajouter",indexController.postIndex)
 
 module.exports = router;
