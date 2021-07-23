e0b38716f9fe8398cc8d05d2c2cb367f3a25461d
sabdesamie <abdesamieselama@gmail.com>
2021-07-23 15:30:52 +0100

    Terminate

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const methodOverride = require("method-override")
 const mongoose = require('mongoose');
 const indexRoutes = require("./routes/index");
 
-mongoose.connect('mongodb+srv://<user>:<password>@cluster0.7ayno.mongodb.net/test',{useNewUrlParser:true}).then(() => {
+mongoose.connect('mongodb+srv://abdessamie:AN73RFXKPT5m3YU1@cluster0.7ayno.mongodb.net/test',{useNewUrlParser:true}).then(() => {
 	console.log('DB connection successfull');
 });
 
-const port = 3000;
+const port = process.env.PORT | 3000;
 const app = express();
 
 
